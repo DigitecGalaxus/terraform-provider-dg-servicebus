70cbd69281b08e4bba717fd5870958bbe59bdc3c
Oliver Morf <Oliver.Morf@digitecgalaxus.ch>
2023-07-28 15:30:32 +0200

    add examples

--- examples/dg-servicebus/main.tf
+++ examples/dg-servicebus/main.tf
@@ -1,29 +1,26 @@
 terraform {
   required_providers {
-    nservicebus = {
-      source = "hashicorp.com/edu/nservicebus"
+    dgservicebus = {
+      source = "hashicorp.com/edu/dg-servicebus"
     }
   }
-
-  required_version = ">= 1.1.0"
 }
 
-
-provider "nservicebus" {
+provider "dgservicebus" {
   azure_servicebus_hostname = "terraform-provider-test.servicebus.windows.net"
 }
 
-resource "nservicebus_endpoint" "dev" {
+
+resource "dgservicebus_endpoint" "dev" {
   endpoint_name = "dg-nservicebus-test-endpoint"
-  resource_group = "Messaging-Dev"
-  topic_name = "bundle-1"
+  topic_name    = "bundle-1"
   subscriptions = [
     "Dg.SalesOrder.V1.SalesOrderCreated",
     "Dg.SalesOrder.V1.A",
     "Dg.SalesOrder.V1.AAA",
   ]
   queue_options = {
-    enable_partitioning = false,
+    enable_partitioning   = false,
     max_size_in_megabytes = 5120,
   }
 }
