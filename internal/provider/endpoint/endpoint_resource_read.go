dee94cadcda4cfb1a6ea9ca685b39ca013292c5e
Michael <michi.straub@icloud.com>
2023-09-12 18:31:11 +0200

    Automatically import rough subscriptions

--- internal/provider/endpoint/endpoint_resource_read.go
+++ internal/provider/endpoint/endpoint_resource_read.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"terraform-provider-dg-servicebus/internal/provider/asb"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/messaging/azservicebus/admin"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
@@ -18,37 +19,20 @@ func (r *endpointResource) Read(ctx context.Context, req resource.ReadRequest, r
 		return
 	}
 
-	model := state.ToAsbModel()
+	previousState := state
 
-	state.QueueExists = types.BoolValue(true)
-	state.EndpointExists = types.BoolValue(true)
-	state.ShouldCreateQueue = types.BoolValue(false)
-	state.ShouldCreateEndpoint = types.BoolValue(false)
 	state.ShouldUpdateSubscriptions = types.BoolValue(false)
 	state.HasMalformedFilters = types.BoolValue(false)
 
-	var success bool
-
-	success = r.updateEndpointQueueState(ctx, model, &state, resp)
-	if !success {
+	if !r.syncQueueState(ctx, &previousState, &state, resp) {
 		return
 	}
 
-	endpointExists, success := r.updateEndpointState(ctx, model, &state, resp)
-	if !success {
+	if !r.syncSubscriptionState(ctx, &previousState, &state, resp) {
 		return
 	}
 
-	// There are no subscriptions to check if the endpoint does not exist
-	if endpointExists {
-		success = r.updateEndpointSubscriptionState(ctx, model, &state)
-		if !success {
-			return
-		}
-	}
-
-	success = r.updateAdditionalQueueState(ctx, &state, resp)
-	if !success {
+	if !r.updateAdditionalQueueState(ctx, &state, resp) {
 		return
 	}
 
@@ -58,8 +42,13 @@ func (r *endpointResource) Read(ctx context.Context, req resource.ReadRequest, r
 	}
 }
 
-func (r *endpointResource) updateEndpointQueueState(ctx context.Context, model asb.EndpointModel, state *endpointResourceModel, resp *resource.ReadResponse) bool {
-	queue, err := r.client.GetEndpointQueue(ctx, model)
+func (r *endpointResource) syncQueueState(
+	ctx context.Context,
+	previousState *endpointResourceModel,
+	updatedState *endpointResourceModel,
+	resp *resource.ReadResponse,
+) bool {
+	queue, err := r.client.GetEndpointQueue(ctx, previousState.ToAsbModel())
 	if err != nil {
 		resp.Diagnostics.AddError(
 			"Error reading Queue",
@@ -68,10 +57,40 @@ func (r *endpointResource) updateEndpointQueueState(ctx context.Context, model a
 		return false
 	}
 
-	if queue == nil {
-		state.QueueExists = types.BoolValue(false)
+	queueExistsInAsb := queue != nil
+	terraformPreviouslyCreatedQueue := previousState.QueueExists.ValueBool()
+
+	if terraformPreviouslyCreatedQueue {
+		if !queueExistsInAsb {
+			tflog.Info(ctx, "Queue exists in Terraform state but not in Azure Service Bus. It will be recreated on next apply.")
+			updatedState.ShouldCreateQueue = types.BoolValue(true)
+			updatedState.QueueExists = types.BoolValue(false)
+			return true
+		}
+
+		applyAsbQueueStateToState(updatedState, queue)
 		return true
 	}
+
+	if !queueExistsInAsb {
+		return true // Wasn't created yet, what we expect
+	}
+
+	tflog.Info(
+		ctx,
+		"Queue exists in Azure Service Bus but not in Terraform state. Importing queue into Terraform state.",
+	)
+
+	applyAsbQueueStateToState(updatedState, queue)
+	return true
+}
+
+func applyAsbQueueStateToState(
+	state *endpointResourceModel,
+	queue *admin.GetQueueResponse,
+) {
+	state.QueueExists = types.BoolValue(true)
+	state.ShouldCreateQueue = types.BoolValue(false)
 
 	maxQueueSizeInMb := *queue.QueueProperties.MaxSizeInMegabytes
 	partitioningIsEnabled := *queue.QueueProperties.EnablePartitioning
@@ -81,42 +100,71 @@ func (r *endpointResource) updateEndpointQueueState(ctx context.Context, model a
 
 	state.QueueOptions.MaxSizeInMegabytes = types.Int64Value(int64(maxQueueSizeInMb))
 	state.QueueOptions.EnablePartitioning = types.BoolValue(partitioningIsEnabled)
-
-	return true
 }
 
-func (r *endpointResource) updateEndpointState(ctx context.Context, model asb.EndpointModel, state *endpointResourceModel, resp *resource.ReadResponse) ( /* Endpoint Exists */ bool /* Success */, bool) {
-	endpointExists, err := r.client.EndpointExists(ctx, model)
+func (r *endpointResource) syncSubscriptionState(
+	ctx context.Context,
+	previousState *endpointResourceModel,
+	updatedState *endpointResourceModel,
+	resp *resource.ReadResponse,
+) bool {
+	endpointExists, err := r.client.EndpointExists(ctx, previousState.ToAsbModel())
 	if err != nil {
 		resp.Diagnostics.AddError(
 			"Error reading Endpoint",
 			"Could not read if an Endpoint exists, unexpected error: "+err.Error(),
 		)
 
-		return false, false
+		return false
+	}
+
+	terraformPreviouslyCreatedEndpoint := previousState.EndpointExists.ValueBool()
+
+	if terraformPreviouslyCreatedEndpoint {
+		if !endpointExists {
+			tflog.Info(ctx, "Endpoint exists in Terraform state but not in Azure Service Bus. It will be recreated on next apply.")
+			updatedState.ShouldCreateEndpoint = types.BoolValue(true)
+			updatedState.EndpointExists = types.BoolValue(false)
+			return true
+		}
+
+		updatedState.ShouldCreateEndpoint = types.BoolValue(false)
+		updatedState.EndpointExists = types.BoolValue(true)
+		r.updateEndpointSubscriptionState(ctx, updatedState)
+
+		return true
 	}
 
 	if !endpointExists {
-		state.EndpointExists = types.BoolValue(false)
-		state.Subscriptions = []string{}
+		updatedState.EndpointExists = types.BoolValue(false)
+		updatedState.ShouldCreateEndpoint = types.BoolValue(true)
+		return true // Wasn't created yet, what we expect
 	}
 
-	return endpointExists, true
+	tflog.Info(
+		ctx,
+		"Endpoint exists in Azure Service Bus but not in Terraform state. Importing endpoint into Terraform state.",
+	)
+
+	updatedState.EndpointExists = types.BoolValue(true)
+	updatedState.ShouldCreateEndpoint = types.BoolValue(false)
+
+	r.updateEndpointSubscriptionState(ctx, updatedState)
+	return true
 }
 
 func (r *endpointResource) updateEndpointSubscriptionState(
 	ctx context.Context,
-	loadedState asb.EndpointModel,
-	currentState *endpointResourceModel,
+	updatedState *endpointResourceModel,
 ) bool {
-	azureSubscriptions, err := r.client.GetEndpointSubscriptions(ctx, loadedState)
+	azureSubscriptions, err := r.client.GetEndpointSubscriptions(ctx, updatedState.ToAsbModel())
 	if err != nil {
 		return false
 	}
 
 	updatedSubscriptionState := []string{}
 	for _, azureSubscription := range azureSubscriptions {
-		subscriptionName := asb.TryGetFullSubscriptionNameFromRuleName(loadedState.Subscriptions, azureSubscription.Name)
+		subscriptionName := asb.TryGetFullSubscriptionNameFromRuleName(updatedState.Subscriptions, azureSubscription.Name)
 		if subscriptionName == nil {
 			tflog.Warn(ctx, fmt.Sprintf("Subscription %s not found in state", azureSubscription.Name))
 			// Add to the state, which will delete the resource on apply
@@ -126,14 +174,14 @@ func (r *endpointResource) updateEndpointSubscriptionState(
 
 		if !asb.IsSubscriptionFilterCorrect(azureSubscription.Filter, *subscriptionName) {
 			tflog.Warn(ctx, fmt.Sprintf("Subscription %s has bad filter", azureSubscription.Name))
-			currentState.HasMalformedFilters = types.BoolValue(true)
+			updatedState.HasMalformedFilters = types.BoolValue(true)
 		}
 
 		tflog.Warn(ctx, fmt.Sprintf("Subscription %s is in state as %s", azureSubscription.Name, *subscriptionName))
 		updatedSubscriptionState = append(updatedSubscriptionState, *subscriptionName)
 	}
 
-	currentState.Subscriptions = updatedSubscriptionState
+	updatedState.Subscriptions = updatedSubscriptionState
 	return true
 }
 
